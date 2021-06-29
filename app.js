6959b9e8871bd0f1fe4b3d9ea3da31901b6e7b8e
meshcheriakov.b@nicecode.biz <meshcheriakov.b@nicecode.biz>
2021-06-29 16:44:26 +0300

    working hover sketch

---
+++ app.js
@@ -0,0 +1,28 @@
+const grid = document.querySelector('.grid');
+let squareSize = 8;
+
+createGrid(squareSize);
+
+function createDiv(size) {
+  const div = document.createElement('div');
+  div.classList.add('box');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
+
+  return div;
+}
+
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      grid.appendChild(createDiv(grid.clientWidth / gridSize));
+    }
+  }
+}
+
+grid.addEventListener('mouseover', function (e) {
+  // Add the "active" class to only divs with a "box" class
+  if (e.target.matches('.box')) {
+    e.target.classList.add('active');
+  }
+});
